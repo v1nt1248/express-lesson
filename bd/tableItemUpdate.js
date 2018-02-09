e6d5ec2131ae2bac673e16b3d981151ba26f979e
v1nt1248 <v1nt1248@gmail.com>
2018-02-09 15:43:45 +0300

    tableItemUpdate is edited

--- bd/tableItemUpdate.js
+++ bd/tableItemUpdate.js
@@ -17,29 +17,29 @@ const updateTableItem = function(docClient, item) {
         if (data.Items.length === 0) {
           reject({message: `Item with id ${item.id} isn't found.`})
         } else {
+          let updateExpression = 'set';
+          let expressionAttributeValues = {};
+          for (const key of Object.keys(item)) {
+            if (key !== 'id') {
+              updateExpression = updateExpression + (key !== 'year' ? ` ${key} = :${key},` : ' #yr = :yr,');
+              expressionAttributeValues[(key !== 'year' ? `:${key}` : ':yr')] = item[key];
+            }
+          }
+          updateExpression = updateExpression.slice(0, -1);
           const params = {
             TableName: 'Phones',
             Key: {
               'id': item.id
             },
-            ExpressionAttributeNames:{
-              "#yr": "year"
-            },
-            UpdateExpression: 'set manufacturer = :manufacturer, model = :model, #yr = :yr, photo = :photo, os = :os, screenSize = :screenSize, screenResolution = :screenResolution, ram = :ram, flashMemory = :flashMemory',
-            ExpressionAttributeValues: {
-              ':manufacturer': item.manufacturer,
-              ':model': item.model,
-              ':yr': item.year,
-              ':photo': item.photo,
-              ':os': item.os,
-              ':screenSize': item.screenSize,
-              ':screenResolution': item.screenResolution,
-              ':ram': item.ram,
-              ':flashMemory': item.flashMemory
-            },
+            // ExpressionAttributeNames: {"#yr": "year"},
+            UpdateExpression: updateExpression,
+            ExpressionAttributeValues: expressionAttributeValues,
             ReturnValues: 'UPDATED_NEW'
           }
-          // console.log(params);
+          if ('year' in item) {
+            params.ExpressionAttributeNames = {"#yr": "year"};
+          }
+          console.log(params);
           docClient.update(params, function(err, reply) {
             if (err) {
               reject(err);
